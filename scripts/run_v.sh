aaaf6090bdcf0b9db5efc353f53d7f300b152b02
juliaolivieri <jolivieri@pacific.edu>
2024-08-12 06:02:42 -0700

    add new run versions for vectorize

--- scripts/run_v.sh
+++ scripts/run_v.sh
@@ -9,32 +9,18 @@ METACOLUMN="age"
 OUTPATH_DESEQ="~/Desktop/Research/equivalence_testing_output/scripts/output/deseq_normalization/${SAVENAME}"
 
 DELTA=1
-INFILE_EQUIV="${OUTPATH_DESEQ}_deseq2_normalized_counts.csv"
+INFILE="${OUTPATH_DESEQ}_deseq2_normalized_counts.csv"
 
 OUTPATH_EQUIV=~/Desktop/Research/equivalence_testing_output/scripts/output/equiv_test_vectorized/
 
 date
-a="Rscript deseq_normalization.R \
-	${INFILE} \
-	${META} \
-	${METACOLUMN} \
-	${OUTPATH_DESEQ}"
-echo $a
-eval $a
-
-date
-echo "normalized"
-
-b="python equiv_test_vectorized.py \
+python equiv_test_vectorized.py \
 	--savename $SAVENAME \
 	--outpath $OUTPATH_EQUIV \
-	--infile $INFILE_EQUIV \
+	--infile $INFILE \
 	--delta $DELTA \
 	--meta ${META} \
-	--condition ${METACOLUMN}"
-
-echo $b
-eval $b
+	--condition ${METACOLUMN}
 
 date
 echo "complete"
